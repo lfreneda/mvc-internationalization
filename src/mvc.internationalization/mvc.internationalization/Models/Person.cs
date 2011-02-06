2531b4350d96de2da757d277dbdf08a340b1dc1d
Luiz Freneda <luiz@freneda.com.br>
2011-02-06 13:59:25 -0200

    adding person model

---
+++ src/mvc.internationalization/mvc.internationalization/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace mvc.internationalization.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int FavoriteNumber { get; set; }
+    }
+}
