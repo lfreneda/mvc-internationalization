e44262ebcd2bcb9ba5a180fe58c6465019e704d2
Luiz Freneda <luiz@freneda.com.br>
2011-02-06 14:14:19 -0200

    adding model dataannotations

--- src/mvc.internationalization/mvc.internationalization/Models/Person.cs
+++ src/mvc.internationalization/mvc.internationalization/Models/Person.cs
@@ -1,10 +1,29 @@
-﻿namespace mvc.internationalization.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc.internationalization.Models
 {
     public class Person
     {
+        [Required(ErrorMessageResourceType = typeof(mvc.internationalization.resources.Resources),
+        ErrorMessageResourceName = "FirstNameRequired")]
+        [StringLength(50, ErrorMessageResourceType = typeof(mvc.internationalization.resources.Resources),
+        ErrorMessageResourceName = "FirstNameInvalid")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(mvc.internationalization.resources.Resources),
+        ErrorMessageResourceName = "LastNameRequired")]
+        [StringLength(50, ErrorMessageResourceType = typeof(mvc.internationalization.resources.Resources),
+        ErrorMessageResourceName = "LastNameInvalid")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(mvc.internationalization.resources.Resources),
+        ErrorMessageResourceName = "EmailRequired")]
+        [RegularExpression(".+@.+\\..+", ErrorMessageResourceType = typeof(mvc.internationalization.resources.Resources),
+        ErrorMessageResourceName = "EmailInvalid")]     
         public string Email { get; set; }
+
+        [Range(0, 100, ErrorMessageResourceType = typeof(mvc.internationalization.resources.Resources),
+        ErrorMessageResourceName = "FavoriteNumberInvalid")]
         public int FavoriteNumber { get; set; }
     }
 }
